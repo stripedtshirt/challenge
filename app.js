ecd25c0041e3f28392b5e5bb155aab73800787e4
stripedtshirt <110096514+stripedtshirt@users.noreply.github.com>
2023-01-27 16:19:30 -0800

    Add files via upload

---
+++ app.js
@@ -0,0 +1,25 @@
+const home = document.getElementById("home");
+const about = document.getElementById("about");
+const exit = document.getElementById("exit");
+const blink = document.getElementById("blinkerhome");
+
+let array = [home, about, exit]
+
+for(let i = 0; i<array.length; i++){
+
+function Display(name){
+    this.name = name
+
+        name.addEventListener("click", function(){
+            let the = document.getElementById(`blinker${name.id}`)
+            the.classList.add("active")
+            for(let i = 0; i < array.length; i++){
+                if(array[i].id != name.id){
+                    let removal = document.getElementById(`blinker${array[i].id}`)
+                    removal.classList.remove("active")
+                }
+            }
+        })
+    }
+        Display(array[i])
+    }
